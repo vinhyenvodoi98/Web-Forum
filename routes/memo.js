f56ed34dea1a653909b9c8608125016faf89b105
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2018-07-14 15:07:49 +0700

    a

--- routes/memo.js
+++ routes/memo.js
@@ -4,10 +4,8 @@ var Memo = require('../models/memo')
 
 /* GET memo listing. */
 router.get('/', async function(req, res) {
-  // TODO: get memos by userId
   var memos = await Memo.find({}).exec();
-  
-  //console.log(memos);
+
   res.render('memo/index', {
     memos: memos,
     user: req.user,
@@ -17,7 +15,7 @@ router.get('/', async function(req, res) {
 
 /* GET new memo screen. */
 router.get('/new', function(req, res) {
-  res.render('memo/new',{});
+  res.render('memo/new');
 });
 
 /* POST new memo. */
@@ -31,20 +29,37 @@ router.post('/new', async function(req, res) {
   res.redirect('/memo');
 });
 
-router.get('/edit/:id',function(req,res){
-  Memo.findById(req.param.id);
+router.get('/edit/:id',function(req, res){
   res.render('memo/edit');
 });
 
 router.post('/edit/:id',function(req, res){
   var memo = {};
-  memo.userId=req.user.id;
   memo.title=req.body.title;
   memo.text=req.body.text;
 
-  var query={_id :req.param.id}
-  Memo.update(query,memo);
-  res.redirect('/memo');
+  var query={_id :req.params.id}
+  Memo.update(query,memo, function(err){
+    if (err){
+      console.log(err);
+    }else{
+      res.redirect('/memo');
+    }
+  });
+});
+
+router.get('/:id',function(req, res){
+  res.render('memo/remove');
+})
+
+router.post('/:id',function(req,res){
+  Memo.findOneAndRemove({_id: req.params.id},(err)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect('/memo');
+    }
+  });
 });
 
 
