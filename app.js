996c2c3a5feee5f62a4b3b8814ec0bc5e126d871
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2018-08-18 08:55:17 +0700

    mlab uri

--- app.js
+++ app.js
@@ -42,7 +42,11 @@ passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
 // mongoose
-mongoose.connect('mongodb://vinhyenvodoi98:bishay612@ds123500.mlab.com:23500/passport');
+  
+ 
+var mongodbUri = 'mongodb://vinhyenvodoi98:bishay612@ds123500.mlab.com:23500/passport';
+mongoose.createConnection(mongodbUri);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
